24d761e5c6dd748e79b7ac17b19e0d8cda5edbf3
arslanislam845 <71604350+arslanislam845@users.noreply.github.com>
2021-05-29 02:12:54 +0500

    Assignment completed

--- 6.May28/q3.py
+++ 6.May28/q3.py
@@ -8,4 +8,4 @@ list1=[1,5,67,2,43,6,4,2,2,4,6,2,1,68,5,4 ]
 print("List: ",list1)
 no=int(input("Enter a number to find Index : "))
 ans=find_Index(no,list1)
-print("Index of the given number is : ",Ans)
+print("Index of the given number is : ",ans)
